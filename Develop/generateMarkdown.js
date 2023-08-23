328bfbf22e9927da5c1f6b3ac39d4ac8ed6fcd20
[Elias Montes] <[eliasmontes911@gmail.com]>
2023-08-22 19:00:44 -0700

    completed generateMarkdown.js

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -1,20 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'none') {
+    return ''
+  } else {
+    return `![license](https://img.shields.io/badge/license-${license}-green)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'none') {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'none') {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const markdown = `
+  # ${data.title} ReadME ${renderLicenseBadge(data.license)}
 
-`;
+  ## Table of Contents:
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation:
+
+  - ${data.installation}
+
+  ## Usage:
+
+  - ${data.usage}
+
+  ## License:
+
+  - ${data.license}
+
+  ## Contributing:
+
+  - ${data.contributing}
+
+  ## Tests:
+
+  - ${data.tests}
+
+  ## Questions:
+
+  - Github: ${data.github}/
+  - Email: ${data.email}
+  `
+  return markdown
 }
 
 module.exports = generateMarkdown;
